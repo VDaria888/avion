4ae226757669cc843f6663ab4c2facfa5b7389b9
VDaria888 <167602245+VDaria888@users.noreply.github.com>
2024-04-24 11:28:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const burger=()=>{
+    const btn=document.querySelector('burger svg:last-child')
+    const off=document.querySelector('.offset')
+    const close=document.querySelector('.offset nav svg')
+    btn.addEventListener('click',()=>{
+        off.style.display='block'
+    })
+    close.addEventListener('click',()=>{
+        off.style.display='none'
+    })
+}
+burger()
